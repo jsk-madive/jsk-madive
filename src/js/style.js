e0d561cd5a68720bd10098bb734aa3ccb3bbb131
jsk <js@madive.co.kr>
2021-04-02 18:13:07 +0900

    0402_커밋

--- src/js/style.js
+++ src/js/style.js
@@ -2,52 +2,26 @@
 
 /* Tab Event */
 
-// tabBar
-function tabBar(nowTab, MenuPosX, transition) {
-  $(nowTab).find('.tab-bar').css({
-    left: MenuPosX + 'px',
-    transition: 'left ' + transition + 's'
-  });
-
-}
 
 // tabAnimate
 function tabAnimate() {
   var tabs = $('.tabs');
-  $('.tab-list > li.on').append('<i class="tab-bar"></i>');
 
   $(document).on('click focus', '.tab-list > li', function () {
     $(this).addClass('on').siblings('li').removeClass('on');
 
     var tabIdx = $(this).index();
     $(tabs).siblings('.tab-contbox').children('.tab-cont').eq(tabIdx).show().siblings('.tab-cont').hide();
-
-    var MenuPosX = $(this).position().left,
-      nowTab = $(this).parent('.tab-list');
-
-    tabBar(nowTab, MenuPosX, 0.3);
   });
 }
 
-$(window).on('resize', function () {
-  var tabsCnt = $('.tab-list').length;
-  if (tabsCnt > 0) {
-    $('.tab-list > li.on').each(function () {
-      var nowTab = $(this).parent('.tab-list'),
-        MenuPosX = $(this).position().left;
-      tabBar(nowTab, MenuPosX, 0);
-    });
-  } else {
-    return 0
-  }
-});
 
 
 // listTabScroll
 function listTabScroll() {
   $('.subtab').each(function () {
     $(this).children('li').on({
-      'click': function () {
+      'click': function click() {
         var leftP = $(this).offset().left;
         $(this).parent().animate({
           scrollLeft: leftP
